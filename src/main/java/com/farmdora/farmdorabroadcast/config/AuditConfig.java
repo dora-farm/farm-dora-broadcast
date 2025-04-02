05a5fbdd4344d1ad7d64e3147a8b5b42b90476ad
leedahoon <ekgns0508@gmail.com>
2025-04-02 20:03:36 +0900

    Project Setting

---
+++ src/main/java/com/farmdora/farmdorabroadcast/config/AuditConfig.java
@@ -0,0 +1,9 @@
+package com.farmdora.farmdorabroadcast.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig {
+}
---
+++ src/test/java/com/farmdora/farmdorabroadcast/FarmDoraBroadcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmdora.farmdorabroadcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmDoraBroadcastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
